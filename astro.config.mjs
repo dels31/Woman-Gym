4c73f35820cda853a6bb588241b5aa95678994e0
dels31 <delly.bathoro@outlook.com>
2026-05-18 20:47:34 +0800

    feat: astro vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import node from '@astrojs/node';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+
+// Cek apakah sedang dijalankan di environment Vercel
+const isVercel = process.env.VERCEL === "1";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: "server",
+  adapter: isVercel
+    ? vercel()
+    : node({
+        mode: "standalone",
+      }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
     server: {
       watch: {
-        ignored: ['**/.env.example', '**/README.md']
-      }
-    }
-  }
+        ignored: ["**/.env.example", "**/README.md"],
+      },
+    },
+  },
 });
